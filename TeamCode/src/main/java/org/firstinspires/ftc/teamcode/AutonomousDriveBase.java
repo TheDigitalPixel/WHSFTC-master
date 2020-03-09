60d6bafd48998d0c358c9987f6da71d5454758e4
TheDigitalPixel <jacob.bass01@gmail.com>
2020-03-09 10:35:39 -0400

    Update AutonomousDriveBase.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousDriveBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousDriveBase.java
@@ -75,14 +75,14 @@ public class AutonomousDriveBase extends LinearOpMode {
         motorWheelFR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         motorWheelBL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         motorWheelBR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        motorTray.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        motorTray.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         motorLift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         motorWheelFL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         motorWheelFR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         motorWheelBL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         motorWheelBR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        motorTray.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        motorTray.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         motorLift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         telemetry.addData("Status", "Initialized");
